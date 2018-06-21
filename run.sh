b602eaa525e7f050ebae87bc9c34907f24698e6d
Elixa-Mae Pesa <eapesa@gmail.com>
2018-06-21 22:00:07 +0800

    Updated run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-javac src/ph/eapesa/apps/*.java -d out/terminal/projectplanschedule/
+javac $(find . -name "*.java") -d out/terminal/projectplanschedule/
 java -classpath out/terminal/projectplanschedule/ ph.eapesa.apps.Main
